b947ff2215f34ba00bfd9642ea17d001b801fec2
AleksRomanov <kosmonavt-vavilon@mail.ru>
2021-01-25 06:01:45 +0700

    Корректировки функционала меню. Ховер
    Правка адаптива

--- source/js/scripts/mmenu.js
+++ source/js/scripts/mmenu.js
@@ -1,37 +1,40 @@
 const mmenu = () => {
-    document.addEventListener(
-        "DOMContentLoaded", () => {
-            new Mmenu( "#my-menu", {
-                "setSelected": {
-                    "hover": true,
-                    "parent": true
-                },
-                // navbar: {
-                //     title: '<img src="catalog/view/theme/cedar/image/svg/exit.svg" alt="Выйти из меню">'
-                // },
-                // "extensions": [
-                //     // "position-back",
-                //     "position-front",
-                //     "position-right",
-                //     'pagedim-black'
-                // ],
-                // "pageScroll": true,
-              sidebar: {
-                collapsed: "(min-width: 240px)",
-                expanded: "(min-width: 1440px)"
-              },
-                pageScroll: {
-                    "scroll": true
-                }
-            }, {
-                pageScroll: {
-                    "scrollOffset": 300,
-                    "updateOffset": 50
-                }
-            });
+  // let hamburgerMenu = document.querySelector('.hamburger');
+
+
+  document.addEventListener(
+    "DOMContentLoaded", () => {
+
+      new Mmenu("#my-menu", {
+          "setSelected": {
+            "hover": true,
+            "parent": true
+          },
+          "extensions": [
+            "position-back"
+            // "all": ["theme-white"],
+            // "(max-width: 12px)": ["fullscreen"]
+            // "fullscreen"
+          ],
+          sidebar: {
+            collapsed: "(min-width: 320px)",
+            expanded: "(min-width: 1440px)"
+          }
         }
-    );
+
+        // hooks: {
+        //   "open:start": (panel) => {
+        //     hamburgerMenu.classList.add('is-active');
+        //     console.log("Started opening panel: ");
+        //   },
+        //   "close:start": (panel) => {
+        //     hamburgerMenu.classList.remove('is-active');
+        //     console.log("Finished opening panel: ");
+        //   }
+        // },
+      );
+    }
+  );
 };
 
 export {mmenu};
-
